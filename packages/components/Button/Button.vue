e34d336757fb37832facbb0319e8e39b69f8b016
wuyongfa <wuyongfa@formssi.com>
2024-07-16 23:43:04 +0800

    init(currying-element): 🎉 项目初始化

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'CButton',
+})
+</script>
+
+<template>
+    <button style="color: red;width: 200px;height: 300px;">66l2131231</button>
+</template>
---
+++ packages/core/components.ts
@@ -0,0 +1,6 @@
+import {CButton} from '@currying-element/components'
+import type  {Plugin} from 'vue'
+
+export default [
+    CButton
+] as Plugin[]
